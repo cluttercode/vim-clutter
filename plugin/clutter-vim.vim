baf07aa2e72bcf851358d30432ba1da1d675b678
Itay Donanhirsh <itay.donanhirsh@checkr.com>
2021-02-01 10:54:16 -0800

    namedtuple for entries, reorg

--- plugin/clutter-vim.vim
+++ plugin/clutter-vim.vim
@@ -19,12 +19,24 @@ sys.path.insert(0, python_root_dir)
 import clutter
 EOF
 
-function! Resolve()
-    python3 clutter.resolve()
+function! ResolveList()
+    python3 clutter.resolve_list()
 endfunction
 
-command! -nargs=0 Resolve call Resolve()
+function! ResolveNext()
+    python3 clutter.resolve1('n')
+endfunction
 
-map <Leader>cr :Resolve<CR>
+function! ResolvePrev()
+    python3 clutter.resolve1('p')
+endfunction
+
+command! -nargs=0 ClutterResolveList call ResolveList()
+command! -nargs=0 ClutterResolveNext call ResolveNext()
+command! -nargs=0 ClutterResolvePrev call ResolvePrev()
+
+map <Leader>cl :ClutterResolveList<CR>
+map <Leader>cp :ClutterResolvePrev<CR>
+map <Leader>cn :ClutterResolveNext<CR>
 
 let g:clutter_plugin_loaded = 1
--- python/clutter.py
+++ python/clutter.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from io import StringIO
 import csv
 import os
@@ -7,23 +8,35 @@ import subprocess
 
 import vim
 
-csv.register_dialect('clutter', delimiter=' ')
+outpattern = re.compile('^(.*):([0-9]+)\.([0-9]+)-([0-9]+)$')
 
-outpattern = re.compile('^(.*):([0-9]+)\.([0-9]+)-[0-9]+$')
+Entry = namedtuple('Entry', ['name', 'path', 'line', 'col', 'endcol', 'attrs'])
 
-def resolve():
+def set_globopts(opts):
+    globopts = opts
+
+def _run(opts=[]):
     row, col = vim.current.window.cursor
 
-    fn = pathlib.PurePath(vim.current.buffer.name).relative_to(os.getcwd())
+    fn = vim.current.buffer.name
+
+    try:
+        fn = pathlib.PurePath(vim.current.buffer.name).relative_to(os.getcwd())
+    except ValueError:
+        print(f'clutter: buffer must be at path relative to current directory')
+        return None
 
     cmd = ["clutter", "-i", "", "r", "--loc", f'{fn}:{row}.{col + 1}']
+
+    cmd.extend(opts)
+
     proc = subprocess.run(cmd, capture_output=True)
 
     stderr = proc.stderr.decode('utf-8').strip()
 
     if proc.returncode != 0:
         print(f'clutter: {stderr}')
-        return
+        return None
 
     if stderr:
         print(f'clutter: {stderr}')
@@ -35,23 +48,57 @@ def resolve():
     vim.command('call setloclist(0, [], "f")')
     vim.command('call setloclist(0, [], "r", {"title": "clutter"})')
 
+    matches = []
+
     for fs in r:
-        if len(fs) != 2:
+        if len(fs) < 2:
             print(f'warning: invalid output line: "{fs}"')
             continue
 
-        name, loc = fs
+        name, loc = fs[0], fs[1]
 
         m = outpattern.match(loc)
         if not m:
             print(f'warning: invalid output line loc: "{loc}"')
             continue
 
-        fn, lnum, col = m.groups()
+        fn, lnum, col, endcol = m.groups()
 
+        matches.append(Entry(name=name, path=fn, line=lnum, col=col, endcol=endcol, attrs=fs[2:]))
+
+    return matches
+
+
+def resolve1(which):
+    matches = _run(opts=[f'-{which}'])
+
+    if len(matches) != 1:
+        _render_list(matches)
+        return
+
+    first = matches[0]
+
+    vim.command(f'keepalt edit {first.path}')
+    vim.command(f'{first.line}')
+    vim.command(f'normal! {first.col}|')
+    vim.command(f'normal! zz')
+
+
+def resolve_list():
+    _render_list(_run())
+
+
+def _render_list(matches):
+    if not matches:
+        return
+
+    vim.command('call setloclist(0, [], "f")')
+    vim.command('call setloclist(0, [], "r", {"title": "clutter"})')
+
+    for m in matches:
         vim.command(
-            'call setloclist(0, [{"filename": "%s", "lnum": %s, "col": %s, "text": "%s"}], "a")' % (
-                fn, lnum, col, name
+            'call setloclist(0, [{"filename": "%s", "lnum": %s, "col": %s, "text": "%s %s"}], "a")' % (
+                m.path, m.line, m.col, m.name, ' '.join(m.attrs),
             )
         )
 
